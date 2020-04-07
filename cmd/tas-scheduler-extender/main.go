a13708825e854da919c6fdf05d50753113d04831
kmuldoon <killian.muldoon@intel.com>
2020-04-07 16:16:42 +0000

    changed default ip for cache to deal with intermittent networking issues / dns problems

--- cmd/tas-scheduler-extender/main.go
+++ cmd/tas-scheduler-extender/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+
 	"github.com/intel/telemetry-aware-scheduling/pkg/cache"
 	"github.com/intel/telemetry-aware-scheduling/pkg/scheduler"
 )
@@ -13,7 +14,7 @@ func main() {
 	flag.StringVar(&port, "port", "9001", "port on which the scheduler extender will listen")
 	flag.StringVar(&certFile, "cert", "/etc/kubernetes/pki/ca.crt", "cert file extender will use for authentication")
 	flag.StringVar(&keyFile, "key", "/etc/kubernetes/pki/ca.key", "key file extender will use for authentication")
-	flag.StringVar(&cacheEndpoint, "cacheEndpoint", "http://localhost:8111/cache/", "root at which the cache can be reached for reading")
+	flag.StringVar(&cacheEndpoint, "cacheEndpoint", "http://127.0.0.1:8111/cache/", "root at which the cache can be reached for reading")
 	flag.BoolVar(&unsafe, "unsafe", false, "unsafe instances of telemetry aware scheduler will be served over simple http.")
 	flag.Parse()
 	cacheReader := cache.RemoteClient{}
