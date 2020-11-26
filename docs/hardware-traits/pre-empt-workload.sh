a5234576517650170522555ed200aa4b537eeddf
Togashi <denisio.togashi@intel.com>
2020-11-26 11:46:07 +0000

    Fix shellscript: SC2148
    SC2148: Add a shebang.

--- docs/hardware-traits/pre-empt-workload.sh
+++ docs/hardware-traits/pre-empt-workload.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 kubectl delete -f power-hungry-application.yaml
 
 kubectl get pods cloud-network-function -w
--- docs/hardware-traits/start-watch-cloud-network-function.sh
+++ docs/hardware-traits/start-watch-cloud-network-function.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 kubectl apply -f cloud-native-network-function.yaml
 
 kubectl get pods -w cloud-network-function
