abb984b61ccc9df2bcc28f976a3c0ca8734a6b98
dtogashi <denisio.togashi@intel.com>
2021-02-23 10:15:34 +0000

    Refactor the files to access client cert/key
    Update configure-scheduler.sh to allow certs on mount volume in
    kube-scheduler pod and copy the client cert/key into it.
    Update scheduler-extender-configmap.yaml to the new reference
    values to the cert/key files.
    Add path for ca cert in the deploy.
    Required and verify client cert in scheduler.go
    
    Co-authored-by:    togashidm <togashidm@gmail.com>
    Co-authored-by: killianmuldoon <killian.muldoon@intel.com>

--- cmd/tas-scheduler-extender/main.go
+++ cmd/tas-scheduler-extender/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"flag"
-
 	"github.com/intel/telemetry-aware-scheduling/pkg/cache"
 	"github.com/intel/telemetry-aware-scheduling/pkg/scheduler"
 )
 
 func main() {
-	var kubeConfig, port, certFile, keyFile, cacheEndpoint string
+	var kubeConfig, port, certFile, keyFile, caFile, cacheEndpoint string
 	var unsafe bool
 	flag.StringVar(&kubeConfig, "kubeConfig", "/root/.kube/config", "location of kubernetes config file")
 	flag.StringVar(&port, "port", "9001", "port on which the scheduler extender will listen")
 	flag.StringVar(&certFile, "cert", "/etc/kubernetes/pki/ca.crt", "cert file extender will use for authentication")
 	flag.StringVar(&keyFile, "key", "/etc/kubernetes/pki/ca.key", "key file extender will use for authentication")
-	flag.StringVar(&cacheEndpoint, "cacheEndpoint", "http://127.0.0.1:8111/cache/", "root at which the cache can be reached for reading")
+	flag.StringVar(&caFile, "cacert", "/etc/kubernetes/pki/ca.crt", "ca file extender will use for authentication")
+	flag.StringVar(&cacheEndpoint, "cacheEndpoint", "http://localhost:8111/cache/", "root at which the cache can be reached for reading")
 	flag.BoolVar(&unsafe, "unsafe", false, "unsafe instances of telemetry aware scheduler will be served over simple http.")
 	flag.Parse()
 	cacheReader := cache.RemoteClient{}
 	cacheReader.RegisterEndpoint(cacheEndpoint)
 	schedulerExtender := scheduler.NewMetricsExtender(&cacheReader)
-	schedulerExtender.StartServer(port, certFile, keyFile, unsafe)
+	schedulerExtender.StartServer(port, certFile, keyFile, caFile, unsafe)
 }
--- deploy/extender-configuration/configure-scheduler.sh
+++ deploy/extender-configuration/configure-scheduler.sh
@@ -16,14 +16,26 @@ kubectl create clusterrolebinding scheduler-config-map --clusterrole=configmapge
 ## Remove arguments from Kubernetes Scheduler file if they exist
 sed -i '/^    - --policy-configmap/d' $MANIFEST_FILE
 sed -i '/^  dnsPolicy: ClusterFirstWithHostNet/d' $MANIFEST_FILE
+sed -i '/certs/d' $MANIFEST_FILE
+sed -i '/name: certdir/d' $MANIFEST_FILE
+sed -i '/hostPath/d'  $MANIFEST_FILE
 
-## Add arguments to our kube-scheduler manifest. There are three arguments here:
-## 1) Policy configmap namespace as arg to binary.
+## Copy client cert/key pair into kube-scheduler
+mkdir /etc/certs/
+cp /etc/kubernetes/pki/ca.key /etc/certs/client.key
+cp /etc/kubernetes/pki/ca.crt /etc/certs/client.crt
+
+## Add arguments to our kube-scheduler manifest. The arguments are:
+## 1) Policy configmap extender as arg to binary.
 ## 2) Policy configmap namespace as arg to binary.
 ## 3) dnsPolicy as part of Pod spec allowing access to kubernetes services.
+## 4) Set autorization certs
 
 sed -e "/    - kube-scheduler/a\\
     - --policy-configmap=scheduler-extender-policy\n    - --policy-configmap-namespace=kube-system" $MANIFEST_FILE -i
 sed -e "/spec/a\\
   dnsPolicy: ClusterFirstWithHostNet" $MANIFEST_FILE -i
-
+sed -e "/      readOnly: true/a\\
+    - mountPath: /host/certs\n      name: certdir" $MANIFEST_FILE -i
+sed -e "/  volumes:/a\\
+  - hostPath:\n      path: /etc/certs\n    name: certdir\n  - hostPath:" $MANIFEST_FILE -i
