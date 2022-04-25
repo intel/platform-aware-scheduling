e5e48826939a4d35076346a17dd67b78dd75a2ff
Madalina Lazar <madalina.lazar@intel.com>
2022-04-25 12:23:03 +0100

    Updating Kind configuration to fetch images from our image registry
    In this commit:
    - we configure the runner to have the required configuration to pull images (certs, configs)
    - we fetch the images from the internal registry only if this configuration exists
    - we configure Kind's containerd to use the internal registry

--- .github/scripts/e2e_get_tools.sh
+++ .github/scripts/e2e_get_tools.sh
@@ -25,3 +25,19 @@ echo "retrieving helm"
 curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo "${root}/tmp/helm.tar.gz" "${HELM_STABLE_RELEASE_URL}" 
 tar -zxvf "${root}/tmp/helm.tar.gz" && mv linux-amd64/helm "${root}/tmp/bin/helm" && rm -rf linux-amd64
 chmod +x "${root}/tmp/bin/helm"
+
+IS_LOCAL="false"
+[ -n "$2" ] && IS_LOCAL=$2
+if [ "$IS_LOCAL" == "true" ]; then
+  PROJECT_ROOT_DIR="${root}/../.."
+  KIND_INTERNAL_CERTS_DIR="${PROJECT_ROOT_DIR}/kind-e2e/ca-certificates"
+  # We expect certificates to be already present on the host, in this specific folder. If not exit with an error
+  if  [ ! -d "$KIND_INTERNAL_CERTS_DIR" ] || [ -z "$(ls -A "$KIND_INTERNAL_CERTS_DIR")" ]; then
+    echo "$KIND_INTERNAL_CERTS_DIR doesn't exist or it's empty. Please check host set-up process as this should've happened on runner set-up. Exiting...  "
+    exit 1
+  else
+    echo "$KIND_INTERNAL_CERTS_DIR is not empty. Will continue and will use existing certificates..."
+  fi
+fi
+
+
