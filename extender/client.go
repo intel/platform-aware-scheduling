23f69240ff86f99271c592e8a1a91eff181d61f5
Ukri Niemimuukko <ukri.niemimuukko@intel.com>
2023-02-21 19:45:33 +0200

    fix qps and burst setting
    Signed-off-by: Ukri Niemimuukko <ukri.niemimuukko@intel.com>

--- extender/client.go
+++ extender/client.go
@@ -15,6 +15,11 @@ import (
 
 // GetKubeClient returns the kube client interface with its config.
 func GetKubeClient(kubeConfig string) (kubernetes.Interface, *rest.Config, error) {
+	return GetKubeClientExt(kubeConfig, 0, 0)
+}
+
+// GetKubeClientExt returns the kube client interface with its config. The given Burst and QPS are set into the config.
+func GetKubeClientExt(kubeConfig string, burst int, qps float32) (kubernetes.Interface, *rest.Config, error) {
 	clientConfig, err := rest.InClusterConfig()
 	if err != nil {
 		klog.V(l2).InfoS("not in cluster - trying file-based configuration", "component", "controller")
@@ -25,13 +30,13 @@ func GetKubeClient(kubeConfig string) (kubernetes.Interface, *rest.Config, error
 		}
 	}
 
+	clientConfig.Burst = burst
+	clientConfig.QPS = qps
+
 	kubeClient, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to create kubeClientset %w", err)
 	}
-
-	clientConfig.Burst = 50
-	clientConfig.QPS = 20
 
 	return kubeClient, clientConfig, nil
 }
