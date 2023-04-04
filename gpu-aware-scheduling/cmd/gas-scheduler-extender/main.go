79ff0b9a903a08f554b37488e5c90dcf4389f2c5
Ukri Niemimuukko <ukri.niemimuukko@intel.com>
2023-04-04 18:36:24 +0300

    Allow controlling qps and burst
    This adds flags for setting burst and qps. The go-code defaults
    match the old zero-values which resulted in 10 and 5 being used.
    Deployment default is raised to match new performance enhancements
    of the gpu plugin. Values are capped to 1000 and negatives are not
    accepted. Giving zeroes will result in 10 and 5 being used.
    
    Signed-off-by: Ukri Niemimuukko <ukri.niemimuukko@intel.com>

--- gpu-aware-scheduling/cmd/gas-scheduler-extender/main.go
+++ gpu-aware-scheduling/cmd/gas-scheduler-extender/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/intel/platform-aware-scheduling/extender"
@@ -22,13 +23,17 @@ var (
 )
 
 const (
-	l1 = klog.Level(1)
+	l1             = klog.Level(1)
+	defaultQPS     = 5
+	defaultBurst   = 10
+	maxQPSandBurst = 1000
 )
 
 func main() {
 	var (
 		kubeConfig, port, certFile, keyFile, caFile, balancedRes string
 		enableAllowlist, enableDenylist                          bool
+		burst, qps                                               uint
 	)
 
 	flag.StringVar(&kubeConfig, "kubeConfig", "/root/.kube/config", "location of kubernetes config file")
@@ -39,12 +44,22 @@ func main() {
 	flag.BoolVar(&enableAllowlist, "enableAllowlist", false, "enable allowed GPUs annotation (csv list of names)")
 	flag.BoolVar(&enableDenylist, "enableDenylist", false, "enable denied GPUs annotation (csv list of names)")
 	flag.StringVar(&balancedRes, "balancedResource", "", "enable resource balacing within a node")
+	flag.UintVar(&burst, "burst", defaultBurst, fmt.Sprintf("burst value used with kube client (limited to %d)", maxQPSandBurst))
+	flag.UintVar(&qps, "qps", defaultQPS, fmt.Sprintf("qps value used with kube client (limited to %d)", maxQPSandBurst))
 	klog.InitFlags(nil)
 	flag.Parse()
 
 	klog.V(l1).Infof("%s built on %s with go %s", version, buildDate, goVersion)
 
-	kubeClient, _, err := extender.GetKubeClient(kubeConfig)
+	for _, ptr := range []*uint{&qps, &burst} {
+		if *ptr > maxQPSandBurst {
+			klog.Warningf("Given flag value %d is too high. Limited to %d.", *ptr, maxQPSandBurst)
+			*ptr = maxQPSandBurst
+		}
+	}
+
+	kubeClient, _, err := extender.GetKubeClientExt(kubeConfig, int(burst), float32(qps))
+
 	if err != nil {
 		klog.Error("couldn't get kube client, cannot continue: ", err.Error())
 		os.Exit(1)
